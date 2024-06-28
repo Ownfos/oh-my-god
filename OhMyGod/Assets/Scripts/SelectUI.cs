8ff4ff9ea721bf3120d770712eb004559f26dbf3
cocoamy11 <cocoamy11@naver.com>
2024-06-29 08:36:25 +0900

    implement godText

--- OhMyGod/Assets/Scripts/SelectUI.cs
+++ OhMyGod/Assets/Scripts/SelectUI.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class GodSelection : MonoBehaviour
 {
@@ -9,12 +10,24 @@ public class GodSelection : MonoBehaviour
     public Button godStrange;
     public Button confirmButton;
 
+    public GameObject godGoodText; 
+    public GameObject godEvilText; 
+    public GameObject godStrangeText; 
+
     void Start()
     {
         godGood.onClick.AddListener(() => SelectGod("Good"));
         godEvil.onClick.AddListener(() => SelectGod("Evil"));
         godStrange.onClick.AddListener(() => SelectGod("Strange"));
         confirmButton.onClick.AddListener(ConfirmSelection);
+
+        // EventTrigger 설정
+        AddEventTrigger(godGood.gameObject, OnGodGoodEnter, EventTriggerType.PointerEnter);
+        AddEventTrigger(godGood.gameObject, OnGodGoodExit, EventTriggerType.PointerExit);
+        AddEventTrigger(godEvil.gameObject, OnGodEvilEnter, EventTriggerType.PointerEnter);
+        AddEventTrigger(godEvil.gameObject, OnGodEvilExit, EventTriggerType.PointerExit);
+        AddEventTrigger(godStrange.gameObject, OnGodStrangeEnter, EventTriggerType.PointerEnter);
+        AddEventTrigger(godStrange.gameObject, OnGodStrangeExit, EventTriggerType.PointerExit);
     }
 
     void SelectGod(string godName)
@@ -25,5 +38,51 @@ public class GodSelection : MonoBehaviour
     void ConfirmSelection()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+    void AddEventTrigger(GameObject obj, UnityEngine.Events.UnityAction<BaseEventData> action, EventTriggerType triggerType)
+    {
+        EventTrigger trigger = obj.GetComponent<EventTrigger>();
+        if (trigger == null)
+        {
+            trigger = obj.AddComponent<EventTrigger>();
+        }
+
+        EventTrigger.Entry entry = new EventTrigger.Entry { eventID = triggerType };
+        entry.callback.AddListener(action);
+        trigger.triggers.Add(entry);
+    }
+
+    // GodGood 이벤트 핸들러
+    void OnGodGoodEnter(BaseEventData data)
+    {
+        godGoodText.SetActive(true);
+    }
+
+    void OnGodGoodExit(BaseEventData data)
+    {
+        godGoodText.SetActive(false);
+    }
+
+    // GodEvil 이벤트 핸들러
+    void OnGodEvilEnter(BaseEventData data)
+    {
+        godEvilText.SetActive(true);
+    }
+
+    void OnGodEvilExit(BaseEventData data)
+    {
+        godEvilText.SetActive(false);
+    }
+
+    // GodStrange 이벤트 핸들러
+    void OnGodStrangeEnter(BaseEventData data)
+    {
+        godStrangeText.SetActive(true);
+    }
+
+    void OnGodStrangeExit(BaseEventData data)
+    {
+        godStrangeText.SetActive(false);
     }
 }
