6763f575f3eb6f0388341a391e5ca8a1855ca4ef
cocoamy11 <cocoamy11@naver.com>
2024-06-29 09:27:51 +0900

    debug test

--- OhMyGod/Assets/Scripts/TitleController.cs
+++ OhMyGod/Assets/Scripts/TitleController.cs
@@ -7,11 +7,13 @@ public class TitleController : MonoBehaviour
 {
    public void StartGame()
     {
+        Debug.Log("StartGame button clicked"); 
         SceneManager.LoadScene("GodSelection"); // godSelection 씬으로 전환
     }
 
     public void ExitGame()
     {
+        Debug.Log("ExitGame button clicked");
         Application.Quit(); // 게임 종료
     }
 }
