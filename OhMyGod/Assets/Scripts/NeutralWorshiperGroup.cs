53b45230aa5ca51a3fb21b4abef88f0e150456ed
ownfos <mejhg1234@gmail.com>
2024-06-29 11:30:58 +0900

    Implement propagating neutral NPCs

--- OhMyGod/Assets/Scripts/NeutralWorshiperGroup.cs
+++ OhMyGod/Assets/Scripts/NeutralWorshiperGroup.cs
@@ -1,10 +1,12 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 // 한 명 이상의 민간인 무리의 포교 처리를 담당하는 클래스
 public class NeutralWorshiperGroup : MonoBehaviour
 {
     private WorshiperController[] worshipers;
+    public int NumWorshipers { get => worshipers.Length; }
+
+    public float PropagationDuration {get; set;} = 0;
 
     private void Start()
     {
@@ -13,5 +15,33 @@ public class NeutralWorshiperGroup : MonoBehaviour
         //
         // 나중에 민간인 집단 포교 처리에 유용하게 사용될 것...
         worshipers = GetComponentsInChildren<WorshiperController>();
+    }
+
+    // PlayerController에서 충분한 포교 시간을 확보한 경우 호출하는 함수.
+    // 포교 성공 확률에 따라 이 집단에 속한 중립 npc들의 포교 성공/실패 처리를 개별적으로 판단함.
+    //
+    // 리턴 값으로 포교에 성공한 중립 npc의 수를 반환함.
+    //
+    // successRate: 포교 성공 확률 (독립 시행)
+    // followTarget: 포교에 성공한 경우 따라다닐 오브젝트
+    public int PerformPropagation(float successRate, GameObject followTarget)
+    {
+        int numSuccess = 0;
+        foreach (WorshiperController worshiper in worshipers)
+        {
+            // 포교 성공 => 신도로 편입
+            if (Random.Range(0f, 1f) < successRate)
+            {
+                numSuccess++;
+                worshiper.FollowTarget = followTarget;
+
+                // 그룹으로 묶고 있던 오브젝트 탈출
+                worshiper.gameObject.transform.parent = null;
+                
+                // TODO: 포교 대상의 종교에 맞게 스프라이트 교체하기
+            }
+        }
+
+        return numSuccess;
     }
 }
--- OhMyGod/Assets/Scripts/PlayerController.cs
+++ OhMyGod/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -14,6 +15,16 @@ public class PlayerController : MonoBehaviour
     private InputActions inputActions;
     private Rigidbody2D rb;
     private Vector3 moveDestination;
+
+    // 포교 범위에 들어온 모든 중립 npc 무리
+    private HashSet<NeutralWorshiperGroup> propagationTargetGroups = new();
+
+    // 중립 npc 포교에 필요한 노출 시간
+    // TODO: 종교 특성에 따라 20% 감소 가능하도록 수정
+    private const float REQUIRED_PROPAGATION_TIME = 2f;
+    // 포교 성공 확률.
+    // 포교에 실패하면 중립 npc는 그대로 소멸한다.
+    private const float PROPAGATION_SUCCESS_RATE = 0.5f;
 
     private const float MAX_DASH_GAUGE = 10f;
     private const float DASH_REPLENISH_TIME = 5f;
@@ -80,7 +91,11 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        // 대쉬 게이지
         UpdateDashGauge();
+
+        // 중립 npc 포교 활동
+        UpdatePropagationTime();
     }
 
     // 대쉬 사용 또는 재충전에 의한 게이지 변동 처리
@@ -149,6 +164,68 @@ public class PlayerController : MonoBehaviour
         else
         {
             return moveSpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // 중립 npc 집단(1인 이상)이 포교 범위에 들어왔는지 체크
+        var group = other.gameObject.GetComponentInParent<NeutralWorshiperGroup>();
+        if (group != null)
+        {
+            // TODO: 내가 먼저 포교를 시작한게 맞는지 확인할 것!
+            // 적이 먼저 포교중이었다면 나는 우선 순위가 낮아서 처리하면 안됨
+            propagationTargetGroups.Add(group);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        // 중립 npc 집단(1인 이상)이 포교 범위에서 나갔는지 체크
+        var group = other.gameObject.GetComponentInParent<NeutralWorshiperGroup>();
+        if (group != null)
+        {
+            propagationTargetGroups.Remove(group);
+            group.PropagationDuration = 0f;
+        }
+    }
+
+    // 트리거 범위에 들어온 중립 npc들의 포교 타이머 관리 및 포교 성공 판정 확률 굴리기
+    private void UpdatePropagationTime()
+    {
+        List<NeutralWorshiperGroup> groupsToDelete = new();
+        foreach(NeutralWorshiperGroup group in propagationTargetGroups)
+        {
+            // 얼마나 오랜 시간 포교에 노출되었는가?
+            group.PropagationDuration += Time.deltaTime;
+
+            // TODO: 종교 특성에 따라 필요 시간 단축 가능하도록 수정
+            if (group.PropagationDuration > REQUIRED_PROPAGATION_TIME)
+            {
+                // 포교 성공 판정을 굴린 npc들은 더이상 관리하지 않음 (신도가 되거나 소멸하거나)
+                groupsToDelete.Add(group);
+
+                // TODO: 종교 특성에 따라 성공 확률 달라지도록 수정
+                int numSuccess = group.PerformPropagation(PROPAGATION_SUCCESS_RATE, gameObject);
+
+                // 집단 포교에 단 한명도 성공하지 못한 경우 패널티로 신도수-1 부여
+                if (group.NumWorshipers > 1 && numSuccess == 0)
+                {
+                    // TODO: 내 신도 한 명 없애기
+                    Debug.Log("집단 전도에 완전히 실패해 신도 수가 감소합니다...");
+                }
+            }
+        }
+
+        
+        foreach (var group in groupsToDelete)
+        {
+            // 일단 null reference를 피하기 위해 포교 대상 리스트에서 삭제하고
+            propagationTargetGroups.Remove(group);
+
+            // 나의 신도가 되지 않은 오브젝트를 포함한 중립 npc 무리의 부모 오브젝트를 없애버림.
+            // 난 죽음을 택하겠다!!!
+            Destroy(group.gameObject);
         }
     }
 }
